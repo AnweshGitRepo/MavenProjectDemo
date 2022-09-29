076b1b618684c9bc93879e03fa00911722b120e4
AnweshGitRepo <anwesh.shrikhande84@gmail.com>
2022-09-29 17:40:01 +0530

    Add one Class

---
+++ src/simpleSeleniumProject/ProjectDetails.java
@@ -0,0 +1,11 @@
+package simpleSeleniumProject;
+
+public class ProjectDetails {
+
+	
+	
+	
+	public void M1() {
+		System.out.println("Systemm");
+	}
+}
--- src/simpleSeleniumProject/SeleniumDemo.java
+++ src/simpleSeleniumProject/SeleniumDemo.java
@@ -2,12 +2,40 @@ package simpleSeleniumProject;
 
 public class SeleniumDemo {
 
-	public static void main(String[] args) {
-		
-		
-		
-		
+	// Program to illustrate this keyword
+	// is used to refer current class
+	
+		// instance variable
+		int a = 10;
+
+		// static variable
+		static int b = 20;
+
+		void GFG()
+		{
+			// referring current class(i.e, class RR)
+			// instance variable(i.e, a)
+			this.a = 100;
+
+			System.out.println(a);
+
+			// referring current class(i.e, class RR)
+			// static variable(i.e, b)
+		b = 600;
+
+			System.out.println(b);
+		}
+
+		public static void main(String[] args)
+		{
+			// Uncomment this and see here you get
+			// Compile Time Error since cannot use
+			// 'this' in static context.
+			// this.a = 700;
+			new SeleniumDemo().GFG();
+		}
 	}
+
 	
 	
-}
+
